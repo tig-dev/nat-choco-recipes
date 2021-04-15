05b7223a3925ef8084c36c2ca1f1ced13a391a1b
Cam Skubik-Peplaski <monsterboy2@icloud.com>
2021-04-15 19:44:14 -0400

    fixed a reference

--- src/data/references.js
+++ src/data/references.js
@@ -10,7 +10,7 @@ const reference_list = [
     "https://www.thespruceeats.com/authentic-mole-poblano-recipe-2314013"
   )}.`,
   // 2
-  `"Heberle, Marianna Olszewska. <i>Polish Cooking</i>. HP Trade, 1987. Print.`,
+  `Heberle, Marianna Olszewska. <i>Polish Cooking</i>. HP Trade, 1987. Print.`,
   // 3
   `Pinel, Florian. "Rigó Jancsi: the Princess, the Fiddler, and the Cake." <i>Food Perestroika</i>. November 22, 2019. Web. ${link_template(
     "https://foodperestroika.com/2019/11/22/rigo-jancsi-the-princess-the-fiddler-and-the-cake/"
